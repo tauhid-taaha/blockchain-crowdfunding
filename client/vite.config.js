833a829fecda69c70ea099b133324fa0dc907de4
tauhid-taaha <127302904+tauhid-taaha@users.noreply.github.com>
2025-06-15 20:19:12 +0600

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://blockchain-crowdfunding-4wah.onrender.com',
         changeOrigin: true,
         secure: false
       }
